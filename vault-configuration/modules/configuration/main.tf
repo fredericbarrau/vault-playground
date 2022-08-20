41a7134313826e8812a90178483b24e21d1dec9a
Frédéric Barrau <frederic.barrau@zenika.com>
2022-08-20 17:10:56 +0200

    ADD enable transit secret

--- vault-configuration/modules/configuration/main.tf
+++ vault-configuration/modules/configuration/main.tf
@@ -3,3 +3,13 @@ resource "vault_auth_backend" "userpass" {
   type = "userpass"
   path = "userpass"
 }
+# Mount transit secret engine
+resource "vault_mount" "transit" {
+  path        = "transit"
+  type        = "transit"
+  description = "Default transit secret engine mount"
+
+  options = {
+    convergent_encryption = false
+  }
+}
